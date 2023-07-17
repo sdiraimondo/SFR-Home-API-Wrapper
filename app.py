d49ef317ae470ce36a4b02622df1fadcd2db69ae
Stéphane D <stephanediraimondo@gmail.com>
2023-07-17 23:40:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
+#!flask/bin/python
 from flask import Flask
+from flask import jsonify
+from flask import abort
+from flask import make_response
+from flask import request
+from flask import redirect
+from flask import url_for
+from flask import send_from_directory
 import os
+import time
 from urllib import request
 from http.cookiejar import CookieJar
 from urllib.parse import urlencode
@@ -10,8 +19,8 @@ import homesfr
 app = Flask(__name__)
 
 # Login
-username = %USER%
-password = %PASSWORD%
+USERNAME = %USER%
+PASSWORD = %PASSWORD%
 
 # Modes utilisables
 MODE_OFF = 0
@@ -29,17 +38,34 @@ PRESENCE_CAMERA_DETECTOR = 'PIR_CAMERA'			# https://boutique.home.sfr.fr/camera
 TEMPHUM_SENSOR = 'TEMP_HUM'				# https://boutique.home.sfr.fr/thermometre
 ONOFF_PLUG = 'ON_OFF_PLUG'				# https://boutique.home.sfr.fr/prise-commandee-connectee-legrand
 
-@app.route("/api/hello/")
+@app.route('/api/hello/')
 def hello():
-    return "Home by SFR APIR Wrapper"
+    return "Home by SFR Bridge"
+
+@app.route('/api/meteo/')
+def meteo():
+    dictionnaire = {
+        'type': 'Prévision de température',
+        'valeurs': [24, 24, 25, 26, 27, 28],
+        'unite': "degrés Celcius"
+    }
+    return jsonify(dictionnaire)
 
 @app.route('/api/get_alarm_mode/')
 def get_mode():
-    homesfr.__init__ (username, password, None, False, True)
+    homesfr.__init__ (USERNAME, PASSWORD, None, False, True)
     if homesfr.login=True:
         mode = homesfr.get_mode()
         return mode
     return (None)  
+
+@app.errorhandler(400)
+def not_complete(error):
+    return make_response(jsonify({'error' : 'request not complete'}), 400)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
